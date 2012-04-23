9896da008275f0a9c6662ecb0b6644f412bc7868
Sage LaTorra <sagelt@gmail.com>
2012-04-23 12:43:02 -0700

    Fix introductory text

--- connectfour.py
+++ connectfour.py
@@ -223,11 +223,11 @@ class ConnectFour:
 		self._game = ConnectFourBoard()
 		self._input_func = input_func
 		self.breakloop = False
-		print "Welcome to Connect Four\n"
-		"There are two colors: black and white. Each player plays one color.\n"
-		"They take turns choosing a column, a piece of their color is dropped into the chosen column.\n"
-		"If there are four pieces of the same color in a row (horizontal, vertical, or diagonal) the player of that color wins.\n"
-		"Black goes first. Good luck!\n"
+		print "Welcome to Connect Four"
+		print "There are two colors: black and white. Each player plays one color."
+		print "They take turns choosing a column, a piece of their color is dropped into the chosen column."
+		print "If there are four pieces of the same color in a row (horizontal, vertical, or diagonal) the player of that color wins."
+		print "Black goes first. Good luck!"
 		if start:
 			self.gameloop()
 	
