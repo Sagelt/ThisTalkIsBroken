7bfab5418617a8890d95d77461c2719e9bf9fedf
Sage LaTorra <sagelt@gmail.com>
2012-04-23 12:41:12 -0700

    Add docstrings

--- tests.py
+++ tests.py
@@ -5,39 +5,61 @@ from cStringIO import StringIO
 import sys
 
 class BoardTests(unittest.TestCase):
+	"""Test the ConnectFourBoard class"""
 	def setUp(self):
+		"""Instantiate a ConnectFourBoard for testing"""
 		self.board = ConnectFourBoard()
-		
+	
 	def playmoves(self, moves):
+		"""For each move in moves, play that move on the board."""
 		for move in moves:
 			self.board.play(move)
-			
-	def switchstdout(self):
-		self.old_stdout = sys.stdout
-		sys.stdout = result = StringIO()
-		return result
-			
+		
+	
 	def verifyboard(self, blacklocations=[], whitelocations=[]):
+		"""Verify that the board reflects the expected plays.
+		
+		All verification is done through asserts, no return value.
+		
+		blacklocations and whitelocations are the (row,column) coordinates
+		of each expected piece. Any location not listed in these collections
+		is expected to be empty."""
 		for location in blacklocations:
 			self.assertEqual(self.board._board[location[0]][location[1]], ConnectFourBoard.blackpiece)
 		for location in whitelocations:
 			self.assertEqual(self.board._board[location[0]][location[1]], ConnectFourBoard.whitepiece)
+		otherlocations = []
+		for col in range(0,ConnectFourBoard._columns):
+			for row in range(0,ConnectFourBoard._rows):
+				if ([row, col] not in blacklocations) and ([row, col] not in whitelocations):
+					otherlocations.append([row, col])
+		
+		for location in otherlocations:
+			self.assertEqual(self.board._board[location[0]][location[1]], ConnectFourBoard.emptyspace)
+		
+	
 			
 	def test_one_move(self):
+		"""Create a board and play one move"""
 		self.board.play(0)
 		self.verifyboard([[5,0]])
+	
+
 
 
 class GameTests(unittest.TestCase):
 	def setUp(self):
+		"""Switch stdout but DO NOT start the game yet"""
 		self.output = self.switchstdout()
-		
+	
 	def switchstdout(self):
+		"""Redirect stdout to a string for verification"""
 		self.old_stdout = sys.stdout
 		sys.stdout = result = StringIO()
 		return result
-		
+	
 	def test_startup(self):
+		"""Test creating a game and quitting"""
 		self.game = ConnectFour(lambda x: "q")
 		result = self.output.getvalue()
 		expectedresult = ("Welcome to Connect Four\n"
@@ -56,6 +78,7 @@ class GameTests(unittest.TestCase):
 		"\n"
 		"It's Black's move.\n")
 		self.assertEqual(result, expectedresult)
+	
 
 if __name__ == '__main__':
     unittest.main()
